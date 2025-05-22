dceaa864deb8cdfa78ef783d626c678a1cfc8731
itsjustchioma <74658446+itsjustchioma@users.noreply.github.com>
2025-05-22 23:43:25 +0100

    resize links on portfolio page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./pages/**/*.html"],
+    content: ["./index.html", "./pages/**/*.html", "./portfolio/**/*.html",],
     theme: {
       extend: {
         fontFamily: {
@@ -13,7 +13,10 @@ module.exports = {
             700: '#B91C1C',
             800: '#991B1B'
           }
-        }
+        },
+        screens: {
+          xlplus: "1250px", // Your custom screen size
+        },
       },
     },
     plugins: [],
